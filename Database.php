d5807013eba3d1d551e4016e719eb6a15825f4c0
ty-low <romainhenri07@gmail.com>
2026-01-05 18:15:56 +0100

    update Carte de france enfin!!!!!!

--- Database.php
+++ Database.php
@@ -7,28 +7,29 @@ ini_set('max_execution_time', 600);
 ///connection classique
 class Database
 {
-    $dsn = 'mysql:host=localhost;dbname=hana2028_LOGA;charset=utf8mb4';
-    $user = 'hana2028_patatoufs';
-    $pass = 'RdAnFK2x436cMVp';
+    private $dsn = 'mysql:host=localhost;dbname=hana2028_LOGA;charset=utf8mb4';
+    private $user = 'hana2028_patatoufs';
+    private $pass = 'RdAnFK2x436cMVp';
     private $host = "localhost";
-    private $username = "root";
-    private $password = "";
+    private $username = "hana2028_patatoufs";
+    private $password = "RdAnFK2x436cMVp";
     private $conn;
 
     public function getConnection()
     {
         $this->conn = null;
         try {
-            $dsn = "mysql:host=localhost;dbname=NOM_BDD;charset=utf8"; //TODO
+            $dsn = "mysql:host=localhost;dbname=hana2028_LOGA;charset=utf8mb4"; //TODO
             $this->conn = new PDO($dsn, $this->username, $this->password);
 
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
+            echo "connected";
         } catch (PDOException $e) {
             echo "Erreur de connexion : " . $e->getMessage();
         }
         return $this->conn;
+
     }
 
 
