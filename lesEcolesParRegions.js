9bec9fd7e6c6a3782074c04d84655639600d4a8e
crysis <crysis.quatre@orange.fr>
2026-01-24 17:02:01 +0100

    les functions

--- lesEcolesParRegions.js
+++ lesEcolesParRegions.js
@@ -2,14 +2,14 @@ let etat_pu = document.querySelectorAll('.public')
 let etat_pr = document.querySelectorAll('.privee')
 console.log(etat_pu,etat_pr)
 function supprimer() {
-    for (let i in etat_pu) {
+    for (let i of etat_pu) {
         try {
             document.querySelector("#liste").remove(i)
         }catch {
 
         }
     }
-    for (let i in etat_pr) {
+    for (let i of etat_pr) {
         try {
             document.querySelector("#liste").remove(i)
         }catch{
@@ -23,19 +23,19 @@ document.querySelector('#Tous').addEventListener("click", (event) => {
     for (let etatPrKey in etat_pr) {
         document.querySelector("#liste").appendChild(etatPrKey)
     }
-    for (let etatPukey in etat_pu) {
+    for (let etatPukey of etat_pu) {
         document.querySelector("#liste").appendChild(etatPukey)
     }
 })
 document.querySelector('#Public').addEventListener("click", (event) => {
     supprimer()
-    for (let etatPukey in etat_pu) {
+    for (let etatPukey of etat_pu) {
         document.querySelector("#liste").appendChild(etatPukey)
     }
 })
 document.querySelector('#Privee').addEventListener("click", (event) => {
     supprimer()
-    for (let etatPrkey in etat_pr) {
+    for (let etatPrkey of etat_pr) {
         document.querySelector("#liste").appendChild(etatPrkey)
     }
 })
