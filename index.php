94d19922cf4391738d1716ca0681d99e06e7a064
ty-low <romainhenri07@gmail.com>
2026-01-29 20:55:48 +0100

    changement sur universite.php

--- index.php
+++ index.php
@@ -165,7 +165,8 @@ if ($_SESSION['profile_image'] != null) {
 
     <a><br>Les universités et formations proviennent du site data.gouv.
         <br>Les dossiers dates de 2026 (sauf master qui date de 2022).<br>
-    Pour toute demande d'ajout d'universités/formations, merci de contacter le support par mail </a>
+    Pour toute demande d'ajout d'universités/formations,<br>
+        merci de contacter le support par mail </a>
     <p id="support">Support: support@loga-univ.fr</p>
     <p id="auteurs">Auteurs:<br>
         <br>
