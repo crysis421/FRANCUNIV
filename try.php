e18975b16ab56ed50cba3811a694d22373acf55f
crysis <crysis.quatre@orange.fr>
2026-01-29 20:40:17 +0100

    les functions

--- try.php
+++ try.php
@@ -48,11 +48,10 @@ function addFormation()
         try {
             if ($row != 0 and !in_array($data[4],$res)) { // On skip la premiere ligne qui sont juste le nom des colonnes
 
-                //$requete = $addData->prepare("UPDATE `formation` SET univ=:univ WHERE nom=:nom");
-                //$requete->bindParam(':nom', $data[4]);
+                $requete = $addData->prepare("INSERT INTO formation(nom,note,univ) VALUES (:nom,0,null)");
+                $requete->bindParam(':nom', $data[4]);
                 echo "Formation : ".$data[4]."<br>";
-                //$requete->bindParam(':univ', $lesUnivs[$data[2]]);
-                //$requete->execute();
+                $requete->execute();
             }
             $row++;
             //On ferme la connection
