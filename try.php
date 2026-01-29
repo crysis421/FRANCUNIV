f574fac094be1554e5626de1fb384e95ef9e53c7
crysis <crysis.quatre@orange.fr>
2026-01-29 20:35:28 +0100

    les functions

--- try.php
+++ try.php
@@ -19,7 +19,6 @@ function addUnivFormation()
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) { //Tant qu'il y a des données dans notre table
         try {
             if ($row != 0 and in_array($data[4],$res)) { // On skip la premiere ligne qui sont juste le nom des colonnes
-                echo "la";
                 $requete = $addData->prepare("UPDATE `formation` SET univ=:univ WHERE nom=:nom");
                 $requete->bindParam(':nom', $data[4]);
                 echo "Formation : ".$data[4]."<br>";
@@ -36,5 +35,33 @@ function addUnivFormation()
     }
     fclose($handle);
 }
-addUnivFormation();
+
+function addFormation()
+{
+    $row = 0; //Nombre de ligne du CSV
+    $addData = Database::connect();
+    $requete = $addData->prepare("SELECT nom from formation");
+    $requete->execute();
+    $res = $requete->fetchAll();
+    $handle = fopen("fr-esr-parcoursup .csv", "r");
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) { //Tant qu'il y a des données dans notre table
+        try {
+            if ($row != 0 and !in_array($data[4],$res)) { // On skip la premiere ligne qui sont juste le nom des colonnes
+
+                //$requete = $addData->prepare("UPDATE `formation` SET univ=:univ WHERE nom=:nom");
+                //$requete->bindParam(':nom', $data[4]);
+                echo "Formation : ".$data[4]."<br>";
+                //$requete->bindParam(':univ', $lesUnivs[$data[2]]);
+                //$requete->execute();
+            }
+            $row++;
+            //On ferme la connection
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            echo "laa<br>";
+        }
+    }
+    fclose($handle);
+}
+addFormation();
 
