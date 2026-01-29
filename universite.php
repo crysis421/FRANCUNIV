74ac55539daecb5e63a08c456bd09f5d722b98f5
ty-low <romainhenri07@gmail.com>
2026-01-29 21:10:06 +0100

    changement sur universite.php

--- universite.php
+++ universite.php
@@ -89,16 +89,18 @@ if ($_SESSION['profile_image'] != null) {
 </div>
 <div id="membre">
     <h1 style="color:black "> Membre </h1>
-    <div id="basdepage">
+</div>
 
-        <a><br>Les universités et formations proviennent du site data.gouv.
-            <br>Les dossiers dates de 2026 (sauf master qui date de 2022).<br>
-            Pour toute demande d'ajout d'universités/formations,<br>
-            merci de contacter le support par mail </a>
-        <p id="support">Support: support@loga-univ.fr</p>
-        <p id="auteurs">Auteurs:<br>
-            <br>
-            Romain Henry : Fondateur <br>
-            Kilian Stievenard : Co-fondateur</p>
-    </div>
+<div id="basdepage">
+
+    <a><br>Les universités et formations proviennent du site data.gouv.
+        <br>Les dossiers dates de 2026 (sauf master qui date de 2022).<br>
+        Pour toute demande d'ajout d'universités/formations,<br>
+        merci de contacter le support par mail </a>
+    <p id="support">Support: support@loga-univ.fr</p>
+    <p id="auteurs">Auteurs:<br>
+        <br>
+        Romain Henry : Fondateur <br>
+        Kilian Stievenard : Co-fondateur</p>
+</div>
 </body>
