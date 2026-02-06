0b47bc26d35207862450ce3a53f27810a0376596
ty-low <romainhenri07@gmail.com>
2026-02-06 11:20:05 +0100

    changement sur universite.php

--- avenir.php
+++ avenir.php
@@ -16,5 +16,11 @@ if ($_SESSION['profile_image'] != null) {
 
 <img src="logo-loga.png" alt="LOGA"/>
 <a href="profil.php"><img id="pp" src="<?= htmlspecialchars($avatar) ?>" alt="Photo de profil"></a>
+
+<div>
+    <p>Sur cette page vous pouvait mettre la formation que vous avez et vous pouvez voir tout<br>
+        les formations qui accepte des gens avec cette formation</p>
+</div>
+
 <?php require('menu.php') ?>
 </html>
