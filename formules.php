0199b5a8d2ac222548c06814fe4793ec64a823a1
ty-low <romainhenri07@gmail.com>
2026-02-10 14:14:21 +0100

    changement sur universite.php

--- formules.php
+++ formules.php
@@ -5,10 +5,22 @@
     <link rel="stylesheet" href="math.css">
     <script src="https://kit.fontawesome.com/26007f065f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">
+    <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 </head>
 <body>
 <?php require('menu1.php') ?>
-<div id="titre"><h1>Analyse</h1></div>
+<div id="titre"><h1>Formules</h1></div>
+<div id="dl">
+    <h2>Devellopement Limite en 0</h2><br>
+    <br>
+    <div class="math">
+        \[
+            \e^x = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + x\epsilon(x)
+        \]
+    </div>
+    <p> </p>
+
+</div>
 </body>
 </html>
