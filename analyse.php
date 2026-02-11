1ae4ac4b9b21b7781e32ce5ab4bd0e0ee1c998b1
ty-low <romainhenri07@gmail.com>
2026-02-11 20:53:05 +0100

    changement sur universite.php

--- analyse.php
+++ analyse.php
@@ -19,10 +19,10 @@
 
 <div class="math">
     \[
-    \sum\limits_{k=1}^{+\inf} u_{k}
+    \sum\limits_{k=1}^{+\infty} u_{k}
     \]
     \[
-    \[u_k\] = e^x  \[\lim_{x \to +\infty} e^x = +\infty \] \neq 0
+    \u_k = e^x  \lim_{x \to +\infty} e^x = +\infty  \neq 0
     \]
 
 </div>
