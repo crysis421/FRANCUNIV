222fa0d28e0cef1cef532c69215027e353c44b9a
ty-low <romainhenri07@gmail.com>
2026-02-11 21:20:21 +0100

    changement sur universite.php

--- analyse.php
+++ analyse.php
@@ -29,12 +29,13 @@
 
 </div>
     <h3>Regle de Rieman</h3>
-    <br><p>(En  \[ +\infty -\infty \]) Si il existe un \[ \alpha \]>1 tels que : <br>
+    <br><p>(En   +\infty -\infty ) Si il existe un \[ \alpha \]>1 tels que : <br>
     <div class="math">
         \[
         \lim_{x \to +\infty} x^{\alpha } f(x) = l
+        = >
         \]
-    = >
+
         \[
         \int_{a}^+\infty f(x)dx
         \]
@@ -44,8 +45,8 @@
     <div class="math">
         \[
         \lim_{x \to +\infty} x^{\alpha } f(x) = l
-        \]
         = >
+        \]
         \[
         \int_{0}^a f(x)dx
         \]
