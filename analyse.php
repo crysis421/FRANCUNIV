2ae903af7cb64dae1866c155bc97228ee3e509f4
ty-low <romainhenri07@gmail.com>
2026-02-11 20:45:46 +0100

    changement sur universite.php

--- analyse.php
+++ analyse.php
@@ -5,10 +5,23 @@
     <link rel="stylesheet" href="math.css">
     <script src="https://kit.fontawesome.com/26007f065f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">
+    <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 </head>
 <body>
 <?php require('menu1.php') ?>
 <div id="titre"><h1>Analyse</h1></div>
+<h2>Convergence de serie :</h2><br>
+<br><p> Si le terme generale de la suite ne tend pas <br>
+    vers 0 => la serie ne converge pas  <br>
+<br>
+ Exemple: la série </p>
+
+<div class="math">
+    \[
+    \sum\limits_{k=1}^{+\inf} u_k}
+    \]
+</div>
+<p>    Avec \[u_k\] = e^x </p>
 </body>
 </html>
