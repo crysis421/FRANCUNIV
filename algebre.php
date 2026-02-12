141bfb22cb0b785fa6501163e361ed4b9b098f71
ty-low <romainhenri07@gmail.com>
2026-02-12 08:50:29 +0100

    changement sur universite.php

--- algebre.php
+++ algebre.php
@@ -17,7 +17,7 @@
 <p>On dit que "e" l element neure de G si :</p>
 \[ \forall x \in G , x * e = e * x = x \]
 <p>On dit l inverse\symetrique de x est :</p>
-\[ \forall x \in G , \exists y , x * y = y * x = e  &nbsp y  &nbsp note x^{-1} \]
+\[ \forall x \in G , \exists y , x * y = y * x = e  ~ y  ~ note x^{-1} \]
     <h3>Montrez que c est un groupe/sous groupes :</h3>
         <p> Dans la pratique on montre que H est un sous groupe de G avec x.y . la loi de G<br>
             d un groupe connu (exmple un sous groupe de (R,+)). Pour ca on montre que :<br></p>
