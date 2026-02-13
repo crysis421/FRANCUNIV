425ddc6594412dd48752e3531ef22057db5a8ef6
ty-low <romainhenri07@gmail.com>
2026-02-13 21:37:40 +0100

    changement sur universite.php

--- equadiff.php
+++ equadiff.php
@@ -13,7 +13,8 @@
 <div id="titre"><h1>Equa &nbsp diff</h1></div>
 <div id="resol1">
     <h2>Resolution d'une equation diff ordinaire homogene :</h2><br>
-    <br><p> Il faut mettre sous forme résolu </p>
+    <br>
+    <p> Il faut mettre sous forme résolu </p>
     <div class="math">
         \[
         y'(x) + \alpha (x) y(x) = 0
@@ -32,9 +33,15 @@
         \]
     </div>
     <h2>Resolution d un systeme differentiel homogene :</h2>
-    <p> On va trouver la matrice associe au systeme a 2 dimensions et puis on cherche les valeurs propres \( ~\lambda _1 ~ \lambda _2 ~\) puis leurs vecteurs propres associées <br>
-    Les solutions sont de la forme :</p>
- \[ Y(t) = \alpha e^{\lambda _1 t}v_1+\beta e^{\lambda _2 t }v_2\]
+    <p> On va trouver la matrice associe au systeme a 2 dimensions et puis on cherche les valeurs propres \( ~\lambda _1
+        ~ \lambda _2 ~\) ,puis leurs vecteurs propres associées. Les solutions sont de la forme :</p>
+    \[ Y(t) = \alpha e^{\lambda _1 t}v_1+\beta e^{\lambda _2 t }v_2\]
+    <p> Exemple : </p>
+    \( \left\{ \begin{array}
+    y'(t)= ~x(t)
+    x'(t)= ~y(t)
+    \end{array}
+    \rigth
 </div>
 </body>
 </html>
