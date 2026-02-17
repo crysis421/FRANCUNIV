cc44c0a2c9256af3305125519789d3c508d06f93
ty-low <romainhenri07@gmail.com>
2026-02-17 18:18:08 +0100

    changement sur universite.php

--- algebre.php
+++ algebre.php
@@ -17,14 +17,14 @@
 <p>On dit que "e" l element neure de G si :</p>
 \[ \forall x \in G , x * e = e * x = x \]
 <p>On dit l inverse\symetrique de x est :</p>
-\[ \forall x \in G , \exists y , x * y = y * x = e  ~ y  ~ note ~ x^{-1} \]
+\[ \forall x \in G , \exists y , x * y = y * x = e ~ y ~ note ~ x^{-1} \]
 <p> On dit que un est commutatif ou abelien si :</p>
 \[\forall (x,y) \in G² , x*y=y*x\]
-    <h3>Montrez que c est un groupe/sous groupes :</h3>
-        <p> Dans la pratique on montre que H est un sous groupe de G avec x.y . la loi de G<br>
-            d un groupe connu (exmple un sous groupe de (R,+)). Pour ca on montre que :<br></p>
+<h3>Montrez que c est un groupe/sous groupes :</h3>
+<p> Dans la pratique on montre que H est un sous groupe de G avec x.y . la loi de G<br>
+    d un groupe connu (exmple un sous groupe de (R,+)). Pour ca on montre que :<br></p>
 <p>1) H contient l'élément neutre de G</p>
-           <p> 2) si   &nbsp\( x,y\in H \)&nbsp , alors  &nbsp\( xy \in H\)&nbsp  et  &nbsp\( x^{-1} \in H \)  </p>
+<p> 2) si &nbsp\( x,y\in H \)&nbsp , alors &nbsp\( xy \in H\)&nbsp et &nbsp\( x^{-1} \in H \) </p>
 <h3> Groupes cycliques :</h3>
 <p> C est des groupes qui sont cylcique qui se repetent (exemple: les modulos)</p>
 \[ \frac{\mathbb{Z}}{n\mathbb{Z}} \]
@@ -35,7 +35,40 @@
 \[\forall (x,y) \in G² , \Phi (xy)=\Phi (x) \Phi (y)\]
 <p> Pour un morphisme qui va de A vers B et les elements neutre a et b des groupes A et B alors :</p>
 \[\Phi(a)=b ~ et ~ x \in A ,\Phi (x^{-1})=(\Phi(x))^{-1} \]
-\[ \Phi ~ est ~ un ~ morphisme ~ de ~ G ~ dans ~ A , ~ on ~dit ~ que ~ \Phi ~ est ~ un ~ isomorphisme ~ si ~ il ~ existe ~ \psi ~ de ~ A ~ dans~ G ~ tel ~ que ~ \Phi\circ\psi = ID_A ~~ \psi\circ\Phi = ID_G \]
+\[ \Phi ~ est ~ un ~ morphisme ~ de ~ G ~ dans ~ A , ~ on ~dit ~ que ~ \Phi ~ est ~ un ~ isomorphisme ~ si ~ il ~ existe
+~ \psi ~ de ~ A ~ dans~ G ~ tel ~ que ~ \Phi\circ\psi = ID_A ~~ \psi\circ\Phi = ID_G \]
 <p> Un automorphisme est un isomorphisme de G dans G </p>
+
+<h2>Determinant :</h2>
+<p> Le determinant est une forme n-linéaire alternées et il s ecrit : det(A) avec A une matrice carrée</p>
+<p> Le determinant ce calcul de la forme suivante : </p>
+\[
+\det(A)
+=
+\begin{vmatrix}
+a_{11} & a_{12} & \cdots & a_{1n} \\
+a_{21} & a_{22} & \cdots & a_{2n} \\
+\vdots & \vdots & \ddots & \vdots \\
+a_{n1} & a_{n2} & \cdots & a_{nn}
+\end{vmatrix}
+=
+\sum_{j=1}^{n} a_{1j} C_{1j}
+\]
+<p>Exemple: </p>
+\[ La ~matrice ~A = \begin{pmatrix}
+1 & 2 & 3\\
+4 & 5 & 6 \\
+7 & 8 & 9
+\end{pmatrix}
+ On & va &  avoir & le & calcul & suivant & det(A)= 1 \begin{vmatrix}
+5 & 6 \\
+8 & 9
+\end{vmatrix} 2 \begin{vmatrix}
+4 & 6 \\
+7 & 9
+\end{vmatrix} 3 \begin{vmatrix}
+4 & 5 \\
+7 & 8
+\end{vmatrix} = 1*(5*9-8*6) + 2*(4*9-7*6) + 3*(4*5-7*8)= -3 -12 -108 = -123\]
 </body>
 </html>
