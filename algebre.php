6a235f202cb846cf60b2707caccc24e3ae015b22
ty-low <romainhenri07@gmail.com>
2026-02-17 21:15:54 +0100

    changement sur universite.php

--- algebre.php
+++ algebre.php
@@ -216,17 +216,17 @@ det(f\circ g )=det(f)det(g)
 \text{Definie (separation) : }~~~
 \phi(x,x) \ge0 \Leftrightarrow ~~~ x=0_V ~~~\forall x \in  V \\
 \\
-\text{Matrice de}\phi : \\
+\text{Matrice de }\phi : \\
 \\
-\text{La matrice M_B(}\phi\text{) de }\phi\text{ dans la base B est la matrice n × n dont le coefficient d indice (i, j) (i-eme
-ligne et j-eme colonne, avec i, j} \in \text{{1, . . . , n}) est}\phi\text{(e_i
-, e_j )}\\
+\text{La matrice}~ M_B(\phi)\ ~ de ~ \phi\text{ dans la base B est la matrice n × n dont le coefficient d indice (i, j) (i-eme
+ligne et j-eme colonne, avec i, j} \in \text{{1, . . . , n}) est }\phi(e_i
+, e_j )\\
 \\
 Mat_B(\phi)=\begin{pmatrix}
 \phi(e_1,e_1 & ... & \phi(e_1,e_n) \\
 \phi(e_2,e_1 & ... & \phi(e_2,e_n) \\
 \vdots & \ddots & \vdots \\
-\phi(e_1,e_1 & ... & \phi(e_1,e_n)
+\phi(e_n,e_1) & ... & \phi(e_n,e_n)
 \end{pmatrix}
 
 
