24229ea0735360cb97bafac7d9094ac41543d9ec
ty-low <romainhenri07@gmail.com>
2026-02-17 18:54:41 +0100

    changement sur universite.php

--- algebre.php
+++ algebre.php
@@ -122,10 +122,10 @@ Exemple: </p>
 \end{pmatrix} \\
 ~det(A)~=1*5*9=45 \]
 <br>
-\[\begin{aligned}det(A^{T})~=~det(A) \\
+\[\begin{document}det(A^{T})~=~det(A) \\
 det(I_n)~=~1  \\
 \text{ si A est inversible alors : } det(A^{-1})~=~\frac{1}{det(A})
-\end{aligned}
+\end{document}
 \]
 <p> Soit 2 matrices carrées A et B : </p>
 \[ det(AB)~=~det(A)det(B)\]
